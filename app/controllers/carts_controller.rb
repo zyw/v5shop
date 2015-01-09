2169ca206157aaf2ce3cc354db318b85062dee14
zyw <zyw090111@163.com>
2015-01-09 22:10:28 +0800

    对购物车价格和数量代码进行重构

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -9,6 +9,29 @@ class CartsController < ApplicationController
 	def add
 		pid = params[:id]
 		pids = session[:cart_pids]
+
+		# 产品数量
+		product = Product.find(pid)
+
+		if !pids
+			pids = Hash.new
+			session[:cart_pids] = pids
+		end
+
+		if pids.has_key?(pid)
+			if product.charge_type == 'num'
+				pnum = pids[pid]
+				pids[pid] = pnum.to_i + 1
+			end
+		else
+			if product.charge_type == 'num'
+				pids[pid] = 1
+			else
+				areas = Dict.where(dict_type_id: 3)
+				pids[pid] = areas[0].value
+			end
+		end
+=begin
 		if pids
 			if !pids.include?(pid)
 				pids.push(pid)
@@ -38,6 +61,8 @@ class CartsController < ApplicationController
 				cpsn[pid] = areas[0].value
 			end
 		end
+=end
+
 		redirect_to :carts_list
 	end
 
@@ -45,10 +70,7 @@ class CartsController < ApplicationController
 		pid = params[:id]
 		pids = session[:cart_pids]
 
-		pids.delete(pid) if pids.include?(pid)
-
-		cpsn = session[:cart_ps_num]
-		cpsn.delete(pid) if cpsn.has_key?(pid)
+		pids.delete(pid) if pids.has_key?(pid)
 
 		if pids.empty?
 			render json: "{\"status\":2,\"message\":\"购物车为空了。\"}"
@@ -61,31 +83,33 @@ class CartsController < ApplicationController
 	def change_num
 		key = params[:key]
 		value = params[:value]
-		cpsn = session[:cart_ps_num]
-		cpsn[key] = value
+		pids = session[:cart_pids]
+		pids[key] = value
 		cart_info()
 		render json: "{\"total\":\"#{@total}\",\"status\":1,\"message\":\"改变购物车产品数量成功。\"}"
 	end
 
 	private
 		def cart_info
-			pids = session[:cart_pids]
-			if pids
+			@pids = session[:cart_pids]
+			if @pids
+				pids = []
+				@pids.each_key{ |key| pids.push(key.to_i)}
+
 				@products = Product.find(pids)
 			else
 				@products = []
 			end
-			@cpsn = session[:cart_ps_num]
 
 			@areas = Dict.where(dict_type_id: 3)
 
 			@total = 0
 			@products.each do |product|
 				if product.charge_type == 'num'
-					@total = @total + (@cpsn[(product.id).to_s]).to_i * (product.feeScale).to_i
+					@total = @total + (@pids[(product.id).to_s]).to_i * (product.feeScale).to_i
 				else
 					(0..(@areas.length-1)).each do |i|
-						if @cpsn[product.id.to_s] == (@areas[i].value)
+						if @pids[product.id.to_s] == (@areas[i].value)
 							@total = @total + ((i + 1) * (product.feeScale).to_i)
 						end
 					end
